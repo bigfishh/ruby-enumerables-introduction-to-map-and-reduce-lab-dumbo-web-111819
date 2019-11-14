2789929b4772ce43455a90a9663b78e3f65001f2
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-14 01:33:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,7 +49,12 @@ def reduce_to_total(source_array, starting_point = 0)
   total 
 end 
 
-def 
+def reduce_to_all_true(source_array) 
+  index = 0 
+  while index < source_array.length do 
+    if source_array[index] 
+       true 
+    end 
   
   
   
