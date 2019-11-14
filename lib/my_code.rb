54b1b3bff82dda6475cbd45c57671bd540e079ee
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-14 01:21:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,2 +1,10 @@
 # My Code here....
-def map_to_
+def map_to_negativize(source_array) 
+  index = 0 
+  neg_array = []
+  while index < source_array.length do 
+    neg_array << (source_array[index] * -1)
+    index += 1 
+  end 
+  neg_array
+end 
