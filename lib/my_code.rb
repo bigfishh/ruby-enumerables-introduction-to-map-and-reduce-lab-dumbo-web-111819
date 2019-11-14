58dbd3e2555e1cfde467a6d5714cdbe61adf9e7a
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-14 01:23:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,3 +18,13 @@ def map_to_no_change(source_array)
   end 
   nc_array
 end 
+
+def map_to_double(source_array) 
+  index = 0 
+  double_array = []
+  while index < source_array.length do 
+    double_array << (source_array[index] * 2)
+    index += 1 
+  end 
+  double_array
+end 
